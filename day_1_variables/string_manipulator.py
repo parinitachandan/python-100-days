f23455d1dc9fa56d4432e8b07356d2b51ef77120
parinita <me@parinita.me>
2022-05-23 12:43:06 +0530

    Day 1 - Learning variables, naming convention, basic string operations

---
+++ day_1_variables/string_manipulator.py
@@ -0,0 +1,7 @@
+print("Hello!\n"
+      "I am Parinita\n"
+      "This is an example of printing in different lines using only one print function."
+      "\n")
+
+print("This " + "is " + "an " + "example " + "of " + "combining " + "strings")
+
---
+++ day_1_variables/variables_demo.py
@@ -0,0 +1,10 @@
+name = input("What is your name?")
+print(name)
+
+name_2 = "James"
+James = "1234567890"
+print(name_2 + " = " + James)
+
+name_3="James3"
+
+print()
