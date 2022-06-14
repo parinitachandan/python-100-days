9a6a43aaa3eef4d79614eaf69099711cb392d2bd
parinita <me@parinita.me>
2022-06-14 12:57:46 +0530

    Day 16 - Learning more on Object-Oriented Programming

---
+++ day_17_oop_And_quiz_project/data.py
@@ -0,0 +1,17 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "India is the 7th largest country", "answer": "True"},
+    {"text": "Fish cannot blink", "answer": "True"},
+    {"text": "The five rings on the Olympic flag are interlocking", "answer": "True"},
+    {"text": "Bananas are curved because they grow upwards towards the sun", "answer": "True"},
+    {"text": "There are 14 bones in a human foot", "answer": "False"},
+    {"text": "A baby has more bones than an adult", "answer": "True"},
+    {"text": "An Ostrich as the largest eyes in the world", "answer": "False"},
+    {"text": "There are over 30,000 varieties of apples existing in the world", "answer": "True"},
+    {"text": "The liver is the largest organ in the human body", "answer": "False"},
+    {"text": "Just like humans, dogs can be left-pawed or right-pawed", "answer": "True"},
+    {"text": "A person’s fingernails and hair continue to grow after they die", "answer": "False"}
+]
---
+++ day_17_oop_And_quiz_project/question_model.py
@@ -0,0 +1,5 @@
+class Question:
+    def __init__(self, text, answer):    # the text and answer here is of the question
+        self.text = text
+        self.answer = answer
+
