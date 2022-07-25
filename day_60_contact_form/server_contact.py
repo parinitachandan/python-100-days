1442a881a508cdc305205d2552c941f2b1110ae0
parinita <me@parinita.me>
2022-07-25 09:59:02 +0530

    Committing Changes

--- day_60_contact_form/server_contact.py
+++ day_60_contact_form/server_contact.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
 import os
+import smtplib
+
+from werkzeug.datastructures import ImmutableMultiDict
 
 app = Flask(__name__)
+
+EMAIL = os.getenv("EMAIL")
+PASSWORD = "ekuaowwuijsxttre"
 
 
 @app.route('/')
@@ -13,12 +19,23 @@ def home():
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("index.html", msg_sent=True)
     return render_template("index.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\n" \
+                    f"Name: {name}\n" \
+                    f"Email: {email}\n" \
+                    f"Phone: {phone}\n" \
+                    f"Message:{message}"
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(from_addr=EMAIL,
+                            to_addrs="me@parinita.me",
+                            msg=email_message)
 
 
 if __name__ == "__main__":
